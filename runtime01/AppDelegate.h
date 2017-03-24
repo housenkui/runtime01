ba93f1edd909c935c1f54df244ea4e74a5f30c8c
Daves_Code_Hou <840737320@qq.com>
2017-03-24 10:19:47 +0800

    Initial Commit

---
+++ runtime01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  runtime01
+//
+//  Created by Code_Hou on 2017/3/24.
+//  Copyright © 2017年 侯森魁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
